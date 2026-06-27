2e2da9892cf6902ef1f74409566161d8a28fefb2
muhsin7majeed <muhsin7ak@gmail.com>
2026-06-27 11:36:33 +0000

    refactor: move client friendship hooks

--- client/src/features/friendship/api/use-reject-friend-request.ts
+++ client/src/features/friendship/api/use-reject-friend-request.ts
@@ -1,7 +1,7 @@
-import { useMutation } from '@tanstack/react-query';
-import api from '@/lib/axios-instance';
 import { toaster } from '@/components/ui/toaster';
 import { useErrorHandler } from '@/hooks/use-error-handler';
+import api from '@/lib/axios-instance';
+import { useMutation } from '@tanstack/react-query';
 
 const rejectFriendRequest = async (senderId: string) => {
   const response = await api.post(`/api/friendship/reject-friend-request`, { senderId });
