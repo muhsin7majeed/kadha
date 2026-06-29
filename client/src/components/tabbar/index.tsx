d062a6b972e823b881781414a646505c31b8c68e
muhsin7majeed <muhsin7ak@gmail.com>
2026-06-29 17:18:14 +0000

    Improve app navigation layout

--- client/src/components/tabbar/index.tsx
+++ client/src/components/tabbar/index.tsx
@@ -1,4 +1,4 @@
-import { Icon, Tabs } from '@chakra-ui/react';
+import { Box, Icon, Tabs } from '@chakra-ui/react';
 import { Link, useLocation } from 'react-router';
 import { LuActivity, LuBookmark, LuCheck, LuFolder, LuHeart, LuHouse } from 'react-icons/lu';
 
@@ -40,30 +40,33 @@ const TabBar = () => {
 
   return (
     <>
-      <Tabs.Root variant="enclosed" fitted defaultValue={activeTab} mx="auto" w="full">
-        <Tabs.List borderRadius="full">
-          {TABS.map((tab, index) => {
-            const isFirst = index === 0;
-            const isLast = index === TABS.length - 1;
+      <Tabs.Root variant="enclosed" defaultValue={activeTab} mx="auto" w="max-content" maxW="calc(100vw - var(--chakra-spacing-4))">
+        <Box overflowX="auto" maxW="full">
+          <Tabs.List borderRadius="full" minW="fit-content" whiteSpace="nowrap">
+            {TABS.map((tab, index) => {
+              const isFirst = index === 0;
+              const isLast = index === TABS.length - 1;
 
-            return (
-              <Tabs.Trigger
-                key={tab.label}
-                value={tab.to}
-                asChild
-                borderLeftRadius={isFirst ? 'full' : 'none'}
-                borderRightRadius={isLast ? 'full' : 'none'}
-                title={tab.label}
-              >
-                <Link to={tab.to}>
-                  <Icon size="lg" color="orange.500">
-                    {tab.icon}
-                  </Icon>
-                </Link>
-              </Tabs.Trigger>
-            );
-          })}
-        </Tabs.List>
+              return (
+                <Tabs.Trigger
+                  key={tab.label}
+                  value={tab.to}
+                  asChild
+                  borderLeftRadius={isFirst ? 'full' : 'none'}
+                  borderRightRadius={isLast ? 'full' : 'none'}
+                  flexShrink={0}
+                  title={tab.label}
+                >
+                  <Link to={tab.to}>
+                    <Icon size="lg" color="orange.500">
+                      {tab.icon}
+                    </Icon>
+                  </Link>
+                </Tabs.Trigger>
+              );
+            })}
+          </Tabs.List>
+        </Box>
       </Tabs.Root>
     </>
   );
