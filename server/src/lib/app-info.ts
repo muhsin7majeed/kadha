c743e19b7c7a510968e5ddf844ba3e932982bcd0
muhsin7majeed <muhsin7ak@gmail.com>
2026-06-29 16:23:57 +0000

    Release v0.1.2

--- server/src/lib/app-info.ts
+++ server/src/lib/app-info.ts
@@ -10,8 +10,8 @@ export function getAppVersion() {
     const packageJsonPath = path.join(process.cwd(), 'package.json');
     const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8')) as PackageJson;
 
-    return packageJson.version || '0.1.1';
+    return packageJson.version || '0.1.2';
   } catch {
-    return '0.1.1';
+    return '0.1.2';
   }
 }
